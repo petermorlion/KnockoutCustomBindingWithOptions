77e81f7f87813f336d6ffc57e0008b27ce3ac09d
Peter Morlion <peter.morlion@live.be>
2013-03-13 19:44:50 +0100

    custom binding and wired everything up

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+ko.bindingHandlers.selectedDataAttribute = {
+	init: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+		var dataAttribute = ko.utils.unwrapObservable(valueAccessor().bind);
+		var viewModelProperty = valueAccessor().to;
+		var dataAttributeValue = $(element).find(':selected').attr(dataAttribute);
+		viewModel[viewModelProperty](dataAttributeValue);
+		
+		$(element).change(function() {
+			var dataAttribute = ko.utils.unwrapObservable(valueAccessor().bind);
+			var viewModelProperty = valueAccessor().to;
+			var dataAttributeValue = $(element).find(':selected').attr(dataAttribute);
+			viewModel[viewModelProperty](dataAttributeValue);
+		});
+	},
+	update: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+		var dataAttribute = ko.utils.unwrapObservable(valueAccessor().bind);
+		var viewModelProperty = valueAccessor().to;
+		var value = viewModel[viewModelProperty]();
+		$("option", element).filter(function(option) { return $(option).attr(dataAttribute) === value; }).prop("selected", "selected");
+	}
+}
+
 $(function() {
 	var viewModel = {
-		cityId: ko.observable(1)
+		cityId: ko.observable(1),
+		country: ko.observable()
 	};
 	
 	ko.applyBindings(viewModel);
